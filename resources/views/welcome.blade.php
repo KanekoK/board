f7828600b12498520c9960ba46db8594f8d38583
金子健哉 <kaneko@queserser-j.co.jp>
2017-12-07 18:59:52 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>誰でも掲示板</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+  <h1>掲示板</h1>
+  <div class="contents_block">
+    <dl>
+      <dt>名前</dt>
+      <dd>テキストが入ります</dd>
+    </dl>
+  </div>
+</body>
+</html>
